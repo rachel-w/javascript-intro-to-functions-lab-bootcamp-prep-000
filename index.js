d7e509944f6f15864ee7fbe0311dc03f731d2232
Rachel Wey <rachelwey96@outlook.com>
2018-10-18 08:07:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ function logWhisper(){
 }
 
 function sayHiToGrandma(){
-
+  if (string.toLowerCase() === string){
+    return 'I can\'t hear you!'
+  }
+  if (string.toUpperCase() === string){
+    return 'YES INDEED!'
+  }
+  if (string === 'I love you, Grandma.'){
+    return 'I love you, too.'
+  }
 }
